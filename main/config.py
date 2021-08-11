31328f08a274ada55d700d1c2a5b6bc67219d7f3
inigoval <inigoval@gmail.com>
2021-08-11 11:48:58 +0100

    Added config updating based on initial params

--- main/config.py
+++ main/config.py
@@ -14,3 +14,17 @@ def load_config():
     with open(path, "r") as ymlconfig:
         config = yaml.load(ymlconfig, Loader=yaml.FullLoader)
     return config
+
+
+def update_config(config):
+    """
+    Adjust config values if using rgz data to remove obsolete values
+    """
+    if config["dataset"] == "rgz":
+        del config["data"]["u"]
+
+    if config["dataset"] == "mirabest":
+        del config["cut_threshold"]
+
+    if config["mu"] == -1:
+        config["mu"] = config["data"]["u_frac"]
